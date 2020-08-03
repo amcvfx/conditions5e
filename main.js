b632afe422952d5537bc169f98cde11d784e7398
amcvfx <35099081+amcvfx@users.noreply.github.com>
2020-08-03 10:34:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,63 +2,6 @@
  * An array of status effect icons which can be applied to Tokens
  * @type {Array}
  */
-CONFIG.statusEffects = [
-  "modules/conditions5e/icons/dead.svg",
-  "modules/conditions5e/icons/unconscious.svg",
-  "modules/conditions5e/icons/stunned.svg",
-  "modules/conditions5e/icons/exhaustion1.svg",
-
-  "modules/conditions5e/icons/incapacitated.svg",
-  "modules/conditions5e/icons/paralyzed.svg",
-  "modules/conditions5e/icons/petrified.svg",
-  "modules/conditions5e/icons/exhaustion2.svg",
-
-  "modules/conditions5e/icons/grappled.svg",
-  "modules/conditions5e/icons/restrained.svg",
-  "modules/conditions5e/icons/prone.svg",
-  "modules/conditions5e/icons/exhaustion3.svg",
-
-  "modules/conditions5e/icons/charmed.svg",
-  "modules/conditions5e/icons/frightened.svg",
-  "modules/conditions5e/icons/poisoned.svg",
-  "modules/conditions5e/icons/exhaustion4.svg",
-
-  "modules/conditions5e/icons/blinded.svg",
-  "modules/conditions5e/icons/deafened.svg",
-  "modules/conditions5e/icons/diseased.svg",
-  "modules/conditions5e/icons/exhaustion5.svg"
-];
-  
-// Condition Types
-CONFIG.conditionTypes = {
-  "blinded": "Blinded",
-  "charmed": "Charmed",
-  "dead": "Dead",
-  "deafened": "Deafened",
-  "diseased": "Diseased",
-  "exhaustion": "Exhaustion",
-  "exhaustion1": "Exhaustion Level 1",
-  "exhaustion2": "Exhaustion Level 2",
-  "exhaustion3": "Exhaustion Level 3",
-  "exhaustion4": "Exhaustion Level 4",
-  "exhaustion5": "Exhaustion Level 5",
-  "frightened": "Frightened",
-  "grappled": "Grappled",
-  "incapacitated": "Inacapacitated",
-  "invisible": "Invisible",
-  "paralyzed": "Paralyzed",
-  "petrified": "Petrified",
-  "poisoned": "Poisoned",
-  "prone": "Prone",
-  "restrained": "Restrained",
-  "stunned": "Stunned",
-  "unconscious": "Unconscious",
-  "wounded": "Wounded"
-};
-
-// Replace selected control icons
-CONFIG.controlIcons.visibility = "modules/conditions5e/icons/invisible.svg";
-CONFIG.controlIcons.defeated = "modules/conditions5e/icons/dead.svg";
 
 // Patch CombatTracker to work with token HUD overlay
 Hooks.once("ready", function() {
@@ -77,10 +20,10 @@ Token.prototype._updateHealthOverlay = function(tok) {
   let priorHealth = tok.data.overlayEffect;
   let newHealth = null;
   if ( curHP <= 0 ) {
-    if ( priorHealth === "modules/conditions5e/icons/dead.svg" ) newHealth = priorHealth;
-    else newHealth = "modules/conditions5e/icons/almostdead.svg";
+    if ( priorHealth === "modules/conditions5e/icons/health_dead.png" ) newHealth = priorHealth;
+    else newHealth = "modules/conditions5e/icons/health_dying.png";
   }
-  else if ( curHP / maxHP < 0.5 ) newHealth = "modules/conditions5e/icons/wounded.svg";
+  else if ( curHP / maxHP < 0.5 ) newHealth = "modules/conditions5e/icons/health_critical.png";
   if ( newHealth !== priorHealth ) {
     if ( newHealth === null ) tok.toggleOverlay(priorHealth);
     else tok.toggleOverlay(newHealth);
